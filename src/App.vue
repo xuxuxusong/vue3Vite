376cdec11a33157ee8375f09746872880028eb42
xusong <xusong1@thinvent.com>
2026-03-19 14:41:16 +0800

    b分支

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
 <script setup>
   import { provide, ref, onMounted } from 'vue';
   const arr = ['B分支']
-  const str = '这是A分支的修改'
+  const str = '已合并A分支的修改'
   provide('flag', ref('哈哈看看看'))
   // onMounted(() => {
   //   handleScreenAuto()
